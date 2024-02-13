2380ddffc49965cb36e6ea93748712ebe696508a
GriseoVul <off.sergienko@gmail.com>
2024-02-13 22:12:18 +0300

    #4 Добавлена модель Product С полями Id Name Count Price EnergyValue DateOfManufacture ExpirationDate

---
+++ RecipeCostCalculation/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeCostCalculation.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Count { get; set; }
+        public decimal Price { get; set; }
+        public int? EnergyValue { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfManufacture { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
