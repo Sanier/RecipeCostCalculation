f419828c5614121fa15c63d97e31bdb17f4a6650
Sanier <sanier20@mail.ru>
2024-02-14 17:45:33 +0300

    Добавлена фейковая бд для первоначальной работы и тестирования

---
+++ RecipeCostCalculation.DAL/Repositories/FakeFridgeRepositories.cs
@@ -0,0 +1,32 @@
+﻿using RecipeCostCalculation.DAL.Interfaces;
+using RecipeCostCalculation.Domain.Entities;
+
+namespace RecipeCostCalculation.DAL.Repositories
+{
+    public class FakeFridgeRepositories : IBaseRepositories<FridgeEntity>
+    {
+        private List<FridgeEntity> _fridges;
+        public async Task Create(FridgeEntity entity)
+        {
+            _fridges.Add(entity);
+            await Task.CompletedTask;
+        }
+
+        public async Task Delete(FridgeEntity entity)
+        {
+            _fridges.Remove(entity);
+            await Task.CompletedTask;
+        }
+
+        public IQueryable<FridgeEntity> GetAll()
+        {
+            return _fridges.AsQueryable();
+        }
+
+        public async Task<FridgeEntity> Update(FridgeEntity entity)
+        {
+            await Task.CompletedTask;
+            return entity;
+        }
+    }
+}
---
+++ RecipeCostCalculation.Domain/Models/FridgeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeCostCalculation.Domain.Models
+{
+    public class FridgeModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Count { get; set; }
+        public double Price { get; set; }
+        public double EnergyValue { get; set; }
+        public string DateOfManufacture { get; set; }
+        public string ExpirationDate { get; set; }
+    }
+}
